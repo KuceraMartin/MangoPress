d2d09c8b45cd1043a869190b4a6aea7aad76416a
KrystofM <krystof.mitka@seznam.cz>
2018-01-31 19:47:04 +0100

    Utils: sitetitle fix

--- theme/utils/site_title.php
+++ theme/utils/site_title.php
@@ -11,7 +11,7 @@ function site_title() {
 	} elseif (is_search()) {
 		$title->setHtml('Search for "' . wp_specialchars($s) . '" - ' . get_bloginfo('name'));
 	} elseif (!(is_404()) && (is_single()) || (is_page())) {
-		$title->setHtml(wp_title('', FALSE) . ' - ' . get_bloginfo('name'));
+		$title->setHtml(wp_title('', FALSE));
 	} elseif (is_404()) {
 		$title->setHtml('Not Found - ' . get_bloginfo('name'));
 	} else {
